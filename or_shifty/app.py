4922bee7d0efd6096bb8b005bfc3a4d2ffbebf28
Thomas Maroulis <thomasmaroulis@gmail.com>
2020-01-25 13:56:41 +0000

    Implement evaluation mode
    - This mode will take a given solution, evaluate the constraints
      and objective function against it, and print the output. It is
      intended as a diagnostic tool

--- or_shifty/app.py
+++ or_shifty/app.py
@@ -1,9 +1,9 @@
 import logging
 import sys
 
-from or_shifty.cli import parse_args, write_output
+from or_shifty.cli import Inputs, parse_args, write_output
 from or_shifty.config import Config
-from or_shifty.model import solve
+from or_shifty.model import evaluate, solve
 
 logging.basicConfig(
     stream=sys.stderr, level=logging.INFO, format="%(levelname)-7s - %(message)s",
@@ -11,7 +11,7 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 
-def ops():
+def main() -> None:
     inputs = parse_args()
     configure_logging(inputs.verbose)
     config = Config.build(
@@ -20,16 +20,29 @@ def ops():
         shifts_by_day=inputs.shifts_by_day,
         history=inputs.history,
     )
+
+    if inputs.evaluate:
+        evaluation_mode(inputs, config)
+    else:
+        solving_mode(inputs, config)
+
+
+def evaluation_mode(inputs: Inputs, config: Config) -> None:
+    evaluate(
+        config=config,
+        objective=inputs.objective,
+        constraints=inputs.constraints,
+        solution=inputs.output,
+    )
+
+
+def solving_mode(inputs: Inputs, config: Config) -> None:
     solution = solve(
         config=config, objective=inputs.objective, constraints=inputs.constraints,
     )
-    if inputs.output_path is not None and not inputs.evaluate:
+    if inputs.output_path is not None:
         write_output(inputs.output_path, solution)
 
 
 def configure_logging(verbose=False):
     logging.getLogger().setLevel(logging.DEBUG if verbose else logging.INFO)
-
-
-def main():
-    ops()
